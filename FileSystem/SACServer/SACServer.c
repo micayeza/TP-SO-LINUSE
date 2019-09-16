6ee639998198073b7d6589aaff84b94122ddbc04
ezchain <ezequielechain@gmail.com>
2019-09-16 16:34:20 -0300

    creo proyecto y agrego configuracion para fuse

---
+++ FileSystem/SACServer/SACServer.c
@@ -0,0 +1,12 @@
+/*
+ * SACServer.c
+ *
+ *  Created on: 16 sep. 2019
+ *      Author: utnso
+ */
+#include "SACServer.h"
+
+int main(){
+
+}
+
---
+++ FileSystem/SACServer/SACServer.h
@@ -0,0 +1,19 @@
+/*
+ * SACServer.h
+ *
+ *  Created on: 16 sep. 2019
+ *      Author: utnso
+ */
+
+#ifndef SACSERVER_H_
+#define SACSERVER_H_
+#include <stddef.h>
+#include <stdlib.h>
+#include <fuse.h>
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <time.h>
+
+
+#endif /* SACSERVER_H_ */
